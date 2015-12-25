0c399a6b193d44626e4911d4474227011f53a5ce
Denis1990 <den.bogdanov4@gmail.com>
2015-12-25 15:18:12 +0300

    header nav css phone 320

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $('.sign').click(function(event) {
-    $('.nav__menu').css('display', 'none');
+    $('.nav__menu').hide();
     $('.header__form .form').toggle();
   });
   $('.nav__toggle').click(function(event) {
-    $('.header__form .form').css('display', 'none');
+    $('.header__form .form').hide();
     $('.nav__menu').toggle();
   });
 });
