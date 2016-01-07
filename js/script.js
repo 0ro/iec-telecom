780a50e26984bf2de37ffa86bd8e13ab4e816993
Denis1990 <den.bogdanov4@gmail.com>
2016-01-07 17:40:02 +0300

    сделал адаптив decision_page

--- js/script.js
+++ js/script.js
@@ -10,22 +10,24 @@ $(document).ready(function() {
   });
 
   /*SLICK*/
-   $('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav'
-  });
-  $('.slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: true,
-    centerMode: true,
-    centerPadding:'-7px',
-    focusOnSelect: true
-  });
+  if ($('div').is('.slider-for')) {
+    $('.slider-for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      asNavFor: '.slider-for',
+      dots: true,
+      centerMode: true,
+      centerPadding:'-7px',
+      focusOnSelect: true
+    });
+  };
 
   /*QUESTION-ANSWER*/
   $('.question').click(function(event) {
